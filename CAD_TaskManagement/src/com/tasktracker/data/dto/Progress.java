6b815e8bfb35a74469d3c55c2f094d0238b853bc
P. Pavithra Dharshini <dharshini172815@gmail.com>
2025-05-16 18:36:23 +0530

    Add files via upload

---
+++ CAD_TaskManagement/src/com/tasktracker/data/dto/Progress.java
@@ -0,0 +1,9 @@
+package com.tasktracker.data.dto;
+
+public class Progress {
+    private String userId;
+    private int totalTasks;
+    private int completedTasks;
+    private int activeHabits;
+    private int totalStreaks;
+}
---
+++ CAD_TaskManagement/src/com/tasktracker/data/dto/Reminder.java
@@ -0,0 +1,10 @@
+package com.tasktracker.data.dto;
+
+import java.time.LocalTime;
+
+public class Reminder {
+    private String reminderId;
+    private String userId;
+    private String message;
+    private LocalTime time;
+}
